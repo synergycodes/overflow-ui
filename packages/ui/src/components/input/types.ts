891416e2e60d811432bc856ee3a723b0cdd4adac
wojciechswiatek <wojciech.swiatek@synergycodes.com>
2025-04-07 13:27:03 +0200

    Add input to docusaurus

--- packages/ui/src/components/input/types.ts
+++ packages/ui/src/components/input/types.ts
@@ -2,7 +2,19 @@ import { InputProps as InputBaseProps } from '@mui/base';
 import { ItemSize } from '@ui/shared/types/item-size';
 
 export type InputProps = InputBaseProps & {
+  /**
+   * If true, displays a search icon.
+   */
+
   searchIcon?: boolean;
+  /**
+   * Callback function executed after the clear action.
+   * If provided, a cross icon will be displayed.
+   */
+
   onClear?: () => void;
+  /**
+   * Defines the size of the input.
+   */
   size?: ItemSize;
 };
---
+++ packages/website/docs/components/input/input-docs.tsx
@@ -0,0 +1,23 @@
+import { Input } from '@axiom/ui';
+import { ComponentPage } from '@site/src/components/component-utils/component-page/component-page';
+import exampleCode from '!!raw-loader!./input.example.jsx';
+import React from 'react';
+
+export function InputDocs() {
+  const [value, setValue] = React.useState<string>('Hello World');
+  return (
+    <ComponentPage
+      preview={
+        <Input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          style={{ minWidth: '150px' }}
+          onClear={() => setValue('')}
+        />
+      }
+      cssPath="components/avatar/avatar.module.css"
+      componentPath="components/input/input.tsx"
+      exampleCode={exampleCode}
+    />
+  );
+}
