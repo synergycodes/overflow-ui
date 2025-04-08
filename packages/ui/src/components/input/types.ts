8d281c45f75080e05f4b62d17a20e9b7af131fd8
Wojciech Świątek <47657132+bondzioyo@users.noreply.github.com>
2025-04-08 11:44:53 +0200

    PRD-643-improve-inputs-api (#18)
    * PRD-643-improve-inputs-api
    
    * Remove redundant props
    
    ---------
    
    Co-authored-by: wojciechswiatek <wojciech.swiatek@synergycodes.com>
    Co-authored-by: Piotr Blaszczyk <piotr.blaszczyk@synergycodes.com>

--- packages/ui/src/components/input/types.ts
+++ packages/ui/src/components/input/types.ts
@@ -2,7 +2,9 @@ import { InputProps as InputBaseProps } from '@mui/base';
 import { ItemSize } from '@ui/shared/types/item-size';
 
 export type InputProps = InputBaseProps & {
-  searchIcon?: boolean;
-  onClear?: () => void;
+  /**
+   * Specifies the size of the input field.
+   * Can be 'small', 'medium', or 'large'.
+   */
   size?: ItemSize;
 };
