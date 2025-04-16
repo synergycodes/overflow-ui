c81ad7a305ffb49c60c059e3af7f14323ed790c1
Szymon Tondowski <stdeykun+git@gmail.com>
2025-04-16 07:07:08 +0200

    PRD-631 date picker documentation has been added (#30)
    * PRD-631 date picker documentation added
    
    * PRD-631 generic comment for values of date picker
    
    ---------
    
    Co-authored-by: Szymon Tondowski <szymon.tondowski@synergycodes.com>

---
+++ packages/website/docs/components/date-picker/date-picker-docs.tsx
@@ -0,0 +1,45 @@
+import { DatePicker } from '@axiom/ui';
+import { ComponentPage } from '@site/src/components/component-utils/component-page/component-page';
+import exampleCode from '!!raw-loader!./date-picker.example.jsx';
+
+export function DatePickerDocs() {
+  return (
+    <ComponentPage
+      preview={
+        <div
+          style={{
+            display: 'grid',
+            width: '100%',
+            gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
+            gap: '1rem',
+          }}
+        >
+          <DatePicker
+            type="default"
+            defaultValue={new Date()}
+            valueFormat="DD.MM.YYYY HH:mm"
+          />
+          <DatePicker
+            type="range"
+            defaultValue={[
+              new Date(),
+              new Date(new Date().setDate(new Date().getDate() + 2)),
+            ]} // now - day after tomorrow
+          />
+          <DatePicker
+            type="multiple"
+            defaultValue={[
+              new Date(new Date().setDate(new Date().getDate() - 2)),
+              new Date(),
+              new Date(new Date().setDate(new Date().getDate() + 2)),
+              new Date(new Date().setDate(new Date().getDate() + 4)),
+            ]}
+          />
+        </div>
+      }
+      cssPath="components/date-picker/data-picker-mantine.css"
+      componentPath="components/date-picker/date-picker.tsx"
+      exampleCode={exampleCode}
+    />
+  );
+}
---
+++ packages/website/docs/components/date-picker/date-picker.example.jsx
@@ -0,0 +1,34 @@
+function Page() {
+  return (
+    <div
+      style={{
+        display: 'grid',
+        width: '100%',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
+        gap: '1rem',
+      }}
+    >
+      <DatePicker
+        type="default"
+        defaultValue={new Date()}
+        valueFormat="DD.MM.YYYY HH:mm"
+      />
+      <DatePicker
+        type="range"
+        defaultValue={[
+          new Date(),
+          new Date(new Date().setDate(new Date().getDate() + 2)),
+        ]} // now - day after tomorrow
+      />
+      <DatePicker
+        type="multiple"
+        defaultValue={[
+          new Date(new Date().setDate(new Date().getDate() - 2)),
+          new Date(),
+          new Date(new Date().setDate(new Date().getDate() + 2)),
+          new Date(new Date().setDate(new Date().getDate() + 4)),
+        ]}
+      />
+    </div>
+  );
+}
