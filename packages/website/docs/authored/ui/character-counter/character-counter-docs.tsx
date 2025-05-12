6239f3906e362a4cd959327ddd352a209521c0a6
Dariusz <dariusz.chynek@synergycodes.com>
2025-05-12 12:02:06 +0200

    code-example fix

--- packages/website/docs/authored/ui/character-counter/character-counter-docs.tsx
+++ packages/website/docs/authored/ui/character-counter/character-counter-docs.tsx
@@ -1,5 +1,5 @@
 import { ComponentPage } from '@site/src/components/component-utils/component-page/component-page';
-import exampleCode from '!!raw-loader!@site/docs/code-examples/input.example.tsx';
+import exampleCode from '!!raw-loader!@site/docs/code-examples/character-counter.example.tsx';
 import { CharacterCounterPreview } from './character-counter-preview/character-counter-preview';
 
 export function CharacterCounterDocs() {
