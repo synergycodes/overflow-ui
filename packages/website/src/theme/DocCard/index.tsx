8c5b0a6b354bb977e782b908f972249358c02c4c
Jan Librowski <jan.librowski@synergycodes.com>
2025-05-28 17:44:24 +0200

    Add node panel based doc card

---
+++ packages/website/src/theme/DocCard/index.tsx
@@ -0,0 +1,114 @@
+import { type ReactNode } from 'react';
+import Link from '@docusaurus/Link';
+import {
+  useDocById,
+  findFirstSidebarItemLink,
+} from '@docusaurus/plugin-content-docs/client';
+import { usePluralForm } from '@docusaurus/theme-common';
+import isInternalUrl from '@docusaurus/isInternalUrl';
+import { translate } from '@docusaurus/Translate';
+
+import type { Props } from '@theme/DocCard';
+import type {
+  PropSidebarItemCategory,
+  PropSidebarItemLink,
+} from '@docusaurus/plugin-content-docs';
+
+import styles from './styles.module.css';
+import { NodeDescription, NodePanel } from '@synergycodes/axiom';
+
+function useCategoryItemsPlural() {
+  const { selectMessage } = usePluralForm();
+  return (count: number) =>
+    selectMessage(
+      count,
+      translate(
+        {
+          message: '1 item|{count} items',
+          id: 'theme.docs.DocCard.categoryDescription.plurals',
+          description:
+            'The default description for a category card in the generated index about how many items this category includes',
+        },
+        { count },
+      ),
+    );
+}
+
+function CardContainer({
+  href,
+  children,
+}: {
+  href: string;
+  children: ReactNode;
+}): ReactNode {
+  return (
+    <Link href={href} className={styles['card-container']}>
+      {children}
+    </Link>
+  );
+}
+
+function CardLayout({
+  href,
+  title,
+  description,
+}: {
+  href: string;
+  icon: ReactNode;
+  title: string;
+  description?: string;
+}): ReactNode {
+  return (
+    <CardContainer href={href}>
+      <NodePanel.Root selected={false}>
+        <NodePanel.Header>
+          <NodeDescription label={title} description={description} />
+        </NodePanel.Header>
+      </NodePanel.Root>
+    </CardContainer>
+  );
+}
+
+function CardCategory({ item }: { item: PropSidebarItemCategory }): ReactNode {
+  const href = findFirstSidebarItemLink(item);
+  const categoryItemsPlural = useCategoryItemsPlural();
+
+  // Unexpected: categories that don't have a link have been filtered upfront
+  if (!href) {
+    return null;
+  }
+
+  return (
+    <CardLayout
+      href={href}
+      icon="🗃️"
+      title={item.label}
+      description={item.description ?? categoryItemsPlural(item.items.length)}
+    />
+  );
+}
+
+function CardLink({ item }: { item: PropSidebarItemLink }): ReactNode {
+  const icon = isInternalUrl(item.href) ? '📄️' : '🔗';
+  const doc = useDocById(item.docId ?? undefined);
+
+  return (
+    <CardLayout
+      href={item.href}
+      icon={icon}
+      title={item.label}
+      description={item.description ?? doc?.description}
+    />
+  );
+}
+
+export default function DocCard({ item }: Props): ReactNode {
+  switch (item.type) {
+    case 'link':
+      return <CardLink item={item} />;
+    case 'category':
+      return <CardCategory item={item} />;
+    default:
+      throw new Error(`unknown item type ${JSON.stringify(item)}`);
+  }
+}
