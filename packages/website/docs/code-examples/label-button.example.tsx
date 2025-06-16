921fa7fa41f3b81e3ba3801e14ac1d3b9afe1575
Piotr Blaszczyk <piotr.blaszczyk@synergycodes.com>
2025-06-16 09:38:34 +0200

    Improve buttons

--- packages/website/docs/code-examples/label-button.example.tsx
+++ packages/website/docs/code-examples/label-button.example.tsx
@@ -1,16 +1,20 @@
-function Example() {
+function LabelButtonExample() {
   return (
-    <div style={{ display: 'flex', gap: '8px', flexWrap: 'wrap' }}>
-      <Button variant="primary">Primary Button</Button>
-      <Button variant="secondary">Secondary Button</Button>
-      <Button variant="gray">Gray Button</Button>
-      <Button variant="error">Error Button</Button>
-      <Button variant="warning">Warning Button</Button>
-      <Button variant="success">Success Button</Button>
-      <Button variant="ghost-destructive">Ghost Destructive</Button>
-      <Button variant="primary" isLoading>
-        Loading
-      </Button>
+    <div style={{ display: 'flex', gap: '8px', flexDirection: 'column' }}>
+      <div style={{ display: 'flex', gap: '8px', flexWrap: 'wrap' }}>
+        <Button variant="primary">Primary Button</Button>
+        <Button variant="secondary">Secondary Button</Button>
+        <Button variant="gray">Gray Button</Button>
+        <Button variant="error">Error Button</Button>
+      </div>
+      <div style={{ display: 'flex', gap: '8px', flexWrap: 'wrap' }}>
+        <Button variant="warning">Warning Button</Button>
+        <Button variant="success">Success Button</Button>
+        <Button variant="ghost-destructive">Ghost Destructive</Button>
+        <Button variant="primary" isLoading>
+          Loading
+        </Button>
+      </div>
     </div>
   );
 }
